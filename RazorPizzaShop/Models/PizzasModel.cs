251f4d16bc4db690aa886e1f27af5933836d70dd
Andrew Midthun <midth002@gmail.com>
2022-10-30 17:08:23 -0500

    Adding post method for a custom pizza.

---
+++ RazorPizzaShop/Models/PizzasModel.cs
@@ -0,0 +1,21 @@
+﻿namespace RazorPizzaShop.Models
+{
+    public class PizzasModel
+    {
+        public string ImageTitle { get; set; }
+        public string PizzaName { get; set; }
+        public float BasePrice { get; set; }
+        public bool TomatoSauce { get; set; }
+        public bool Cheese { get; set; }
+        public bool Peperoni { get; set; }
+        public bool Sausage { get; set; }
+        public bool Mushroom { get; set; }
+        public bool CanadianBacon { get; set; }
+        public bool Ham { get; set; }
+        public bool Beef { get; set; }
+        public bool Olives { get; set; }
+        public bool GreenPeppers { get; set; }
+        public bool Pineapple { get; set; }
+        public float FinalPrice { get; set; }
+    }
+}
--- RazorPizzaShop/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizzaShop/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzaShop.Models;
 
 namespace RazorPizzaShop.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+
+        [BindProperty]
+
+        public PizzasModel Pizza { get; set; }
+
+
         public void OnGet()
         {
         }
