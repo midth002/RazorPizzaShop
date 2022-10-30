042e163e7b7831e4db4e68ac643d8d57332f48b4
Andrew Midthun <midth002@gmail.com>
2022-10-30 17:59:17 -0500

    Able to redirect user after posting a custom pizza.

--- RazorPizzaShop/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizzaShop/Pages/Forms/CustomPizza.cshtml.cs
@@ -11,9 +11,31 @@ namespace RazorPizzaShop.Pages.Forms
 
         public PizzasModel Pizza { get; set; }
 
+        public float PizzaPrice { get; set; }   
+   
+
 
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.GreenPeppers) PizzaPrice += 1;
+            if (Pizza.Olives) PizzaPrice += 1;
+            if (Pizza.Peperoni) PizzaPrice += 2;
+            if (Pizza.Sausage) PizzaPrice += 2;
+            if (Pizza.Ham) PizzaPrice += 2;
+            if (Pizza.CanadianBacon) PizzaPrice += 2;
+            if (Pizza.Beef) PizzaPrice += 2;
+            if (Pizza.Pineapple) PizzaPrice += 1;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice});
+
         }
     }
 }
