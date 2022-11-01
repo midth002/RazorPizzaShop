b4b88ff54c0a0a19bc518d8d62d961ceebc5f1f4
Andrew Midthun <midth002@gmail.com>
2022-11-01 18:15:22 -0500

    Build: pizza model choices added to the pizza model class.

--- RazorPizzaShop/Pages/Pizza.cshtml.cs
+++ RazorPizzaShop/Pages/Pizza.cshtml.cs
@@ -1,10 +1,93 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzaShop.Models;
 
 namespace RazorPizzaShop.Pages
 {
     public class PizzaModel : PageModel
     {
+        public List<PizzasModel> fakePizzaDB = new List<PizzasModel>()
+        {
+            new PizzasModel(){
+                ImageTitle="Margerita",
+                PizzaName="Margerita",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=12
+            },
+            new PizzasModel(){
+                ImageTitle="Carbonara",
+                PizzaName="Carbonara",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=12
+            },
+            new PizzasModel(){
+                ImageTitle="Pepperoni",
+                PizzaName="Pepperoni",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=8
+            },
+            new PizzasModel(){
+                ImageTitle="Bolognese",
+                PizzaName="Bolognese",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=8
+            },
+            new PizzasModel(){
+                ImageTitle="Vegetarian",
+                PizzaName="Vegetarian",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=10
+            },
+            new PizzasModel(){
+                ImageTitle="MeatFeast",
+                PizzaName="MeatFeast",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=14
+            },
+            new PizzasModel(){
+                ImageTitle="Hawaiian",
+                PizzaName="Hawaiian",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=8
+            },
+            new PizzasModel(){
+                ImageTitle="Mushroom",
+                PizzaName="Mushroom",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=8
+            },
+            new PizzasModel(){
+                ImageTitle="Seafood",
+                PizzaName="Seafood",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=14
+            }
+
+
+
+
+
+            };
+
+
         public void OnGet()
         {
         }
